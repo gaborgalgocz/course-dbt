74aa084067cbc5073f7ee101355bce5c01acf57a
Gábor Galgócz <ulvrmxsw6@protonmail.com>
2023-02-03 12:46:16 +0000

    replaced old session model with one that uses a macro

--- greenery/models/intermediate/int_session_events.sql
+++ greenery/models/intermediate/int_session_events.sql
@@ -1,20 +1,8 @@
-with events as (
-    select * from {{ ref('stg_postgres_events')  }}
-)
+{{ config(materialized='table') }}
 
-{%- set event_types = dbt_utils.get_column_values(
-    table=ref('stg_postgres_events'),
-    column='event_type')
-    -%}
-
-select 
-    session_id,
-    user_id, 
-    product_id,
-    order_id
-     {%- for event_type in event_types %}
-    , {{ case_agg('event_type', event_type) }} as {{ event_type}}
-    {%- endfor %}
-from events
-group by 1,2,3,4
-
+select
+    user_id,
+    session_id
+    {{ aggregate_event_types() }}
+from {{ ref('stg_postgres_events')}}
+group by 1, 2
--- greenery/models/intermediate/int_session_events_macro.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-select
-    user_id,
-    session_id
-    {{ aggregate_event_types() }}
-from {{ ref('stg_postgres_events')}}
-group by 1, 2
